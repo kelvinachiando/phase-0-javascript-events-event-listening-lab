807b839510b21e3b37d9a9c40983271ed11783ed
kelvinachiando <kelvinostin22gmail.com>
2022-06-13 16:31:44 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 function addingEventListener () {
     const input =document.getElementById(`input`)
-
-
-    function clickAlert (){
-    alert(`I was clicked`)
-}
-
-input.addEventListener('click', clickAlert);
-
+    input.addEventListener('click', function() {
+        alert(`I was clicked`)
+    });
 }
